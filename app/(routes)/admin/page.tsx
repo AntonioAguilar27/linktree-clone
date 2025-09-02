a7caac65218753dd9a586ce2d1b79e41ceb14a5a
pedro-aguilar-gc1 <pedro.aguilar@gc1holding.com>
2025-09-02 10:01:01 -0700

    first commit

---
+++ app/(routes)/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "./components/Header";
+
+export default function Adminpage() {
+  return (
+    <div>
+      AdminPage
+      <Header></Header>
+    </div>
+  );
+}
